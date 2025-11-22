c11b2e10a89b8f2d544908e71e0a3edf9d5ee245
Long <Long012225@gmail.com>
2025-11-22 11:06:04 +0700

    Initial commit with large Unity assets tracked by LFS

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform playerTransform; // Tham chiếu đến Transform của nhân vật người chơi
+    public float sensivity = 2f;
+    public float minXAngle = -30f;
+    public float maxXAngle = 30f;
+    public float minYAngle = -360f;
+    public float maxYAngle = 360f;
+    public float smoothSpeed = 10f;
+
+    public Camera mainCam;
+    public float normalFOV = 60f;
+    public float aimFOV = 35f;
+    public float aimSpeed = 10f;
+
+    private bool isAiming = false;
+
+
+    private float rotationX = 0f;
+    private float rotationY = 0f;
+
+    public Vector3 cameraOffset = new Vector3(0.8f, 1.5f, -3f);
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float characterOffsetY = 0f;
+        float mouseX = Input.GetAxis("Mouse X") * sensivity;
+        float mouseY = Input.GetAxis("Mouse Y") * sensivity;
+
+        rotationY += mouseX;
+        rotationX -= mouseY;
+
+        rotationX = Mathf.Clamp(rotationX, minXAngle, maxXAngle);
+        rotationY = Mathf.Clamp(rotationY, minYAngle, maxYAngle);
+
+        Quaternion targetRotation = Quaternion.Euler(rotationX, rotationY, 0f);
+
+        playerTransform.rotation = Quaternion.Slerp(
+        playerTransform.rotation,
+        Quaternion.Euler(0f, rotationY + characterOffsetY, 0f),
+        smoothSpeed * Time.deltaTime);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, smoothSpeed * Time.deltaTime);
+        transform.position = playerTransform.position + targetRotation * cameraOffset;
+
+        // Nhấn chuột phải → ngắm
+        if (Mouse.current.rightButton.isPressed)
+            isAiming = true;
+        else
+            isAiming = false;
+
+        // Smooth zoom
+        float targetFOV = isAiming ? aimFOV : normalFOV;
+        mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, targetFOV, Time.deltaTime * aimSpeed);
+
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+public class PlayerController : MonoBehaviour
+{
+    private float moveSpeed = 5f;
+    private Rigidbody rb;
+    private Vector2 moveInput;
+    private Animator animator;
+
+    public float mouseSensitivity = 100f;
+    private float rotationY = 0f;
+
+    private bool throwing = false;
+    public float throwTime = 5.0f;  // thời gian animation throw
+
+    IEnumerator ThrowRoutine()
+    {
+        throwing = true;
+        animator.SetBool("isThrowing", true);
+
+        yield return new WaitForSeconds(throwTime);
+
+        animator.SetBool("isThrowing", false);
+        throwing = false;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+   
+    void Update()
+    {
+        float mouseX = Mouse.current.delta.x.ReadValue() * mouseSensitivity * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * mouseX);
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            animator.SetBool("Shoot", true);
+            Debug.Log("Shoot");
+        }
+        else if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            animator.SetBool("Shoot", false);
+        }
+
+        if (Keyboard.current.rKey.wasPressedThisFrame && !throwing)
+        {
+            StartCoroutine(ThrowRoutine());
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = transform.right * moveInput.x + transform.forward * moveInput.y;
+
+        rb.MovePosition(transform.position + movement * moveSpeed * Time.fixedDeltaTime);
+
+        animator.SetFloat("MoveX", moveInput.x);   // A = -1, D = +1
+        animator.SetFloat("MoveY", moveInput.y);   // S = -1, W = +1
+    }
+    public void OnMove(InputValue InputValue)
+    {
+        moveInput = InputValue.Get<Vector2>();
+    }
+}
